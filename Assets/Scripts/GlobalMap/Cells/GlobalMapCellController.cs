751955573059574dd64109164cfc3fd01d26697e
Andrey Anikin <a.anikin@kefirgames.com>
2024-10-10 00:24:10 +0300

    add: switch scene on click gm location

--- Assets/Scripts/GlobalMap/Cells/GlobalMapCellController.cs
+++ Assets/Scripts/GlobalMap/Cells/GlobalMapCellController.cs
@@ -1,4 +1,7 @@
+using System;
 using GlobalMap.Cells.Models;
+using GlobalMap.Locations;
+using ScenesManager;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -15,6 +18,24 @@ namespace GlobalMap.Cells
         public void OnPointerClick(PointerEventData eventData)
         {
             Debug.Log($"Click on cell {_model.Location?.GetDescription().LocationType}");
+            
+            if(_model.Location != null)
+                SwitchScene(_model.Location.GetDescription().LocationType);
+        }
+
+        private static void SwitchScene(GlobalMapLocationType locationType)
+        {
+            switch (locationType)
+            {
+                case GlobalMapLocationType.Home:
+                    GameController.SwitchScene(SceneType.City);
+                    break;
+                case GlobalMapLocationType.Enemy:
+                    GameController.SwitchScene(SceneType.Battle);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(locationType), locationType, null);
+            }
         }
 
         public void Activate(GlobalMapCellModel model)
